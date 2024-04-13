257bc38716732d20039fb96f614d05eb5e97221e
adanshul <31799179+adanshul@users.noreply.github.com>
2024-04-13 15:21:30 -0400

    Update app.js
    Vulnerability added to app.js

--- app/app.js
+++ app/app.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const { exec } = require('child_process');
 
 app.get('/', (req, res) => {
   res.send('Hint: Check the commit messages for clues!');
+});
+
+app.get('/exploit', (req, res) => {
+  const commitMessage = req.headers['x-commit-message'];
+  exec(commitMessage, (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Error executing command: ${err}`);
+      res.status(500).send('Error executing command');
+    } else {
+      res.send(`Command output: ${stdout}`);
+    }
+  });
 });
 
 app.listen(port, () => {
