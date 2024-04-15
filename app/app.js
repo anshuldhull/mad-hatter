cf31518361df00d7905fd6580927d978a9ad13e9
adanshul <31799179+adanshul@users.noreply.github.com>
2024-04-15 19:44:36 -0400

    Update app.js
    created generateOneTimeUrl function

--- app/app.js
+++ app/app.js
@@ -2,21 +2,67 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const { exec } = require('child_process');
+const AWS = require('aws-sdk');
+
+// Configure the AWS SDK
+AWS.config.update({
+  region: 'us-east-2', // Replace with your desired AWS region
+});
+
+const secretsManager = new AWS.SecretsManager();
+
+async function generateOneTimeUrl(flag) {
+  // Store the flag in AWS Secrets Manager
+  const secretParams = {
+    Name: 'ctf-flag',
+    SecretString: flag,
+  };
+
+  return new Promise((resolve, reject) => {
+    secretsManager.createSecret(secretParams, (err, data) => {
+      if (err) {
+        console.error('Error storing flag in Secrets Manager:', err);
+        reject(err);
+      } else {
+        // Generate a one-time use URL
+        const oneTimeUrl = `http://${process.env.APP_HOST}/retrieve-flag?id=${data.VersionId}`;
+        resolve(oneTimeUrl);
+      }
+    });
+  });
+}
 
 app.get('/', (req, res) => {
   res.send('Hint: Check the commit messages for clues!');
 });
 
-app.get('/exploit', (req, res) => {
+app.get('/exploit', async (req, res) => {
   const commitMessage = req.headers['x-commit-message'];
-  exec(commitMessage, (err, stdout, stderr) => {
+  exec(commitMessage, async (err, stdout, stderr) => {
     if (err) {
       console.error(`Error executing command: ${err}`);
       res.status(500).send('Error executing command');
     } else {
-      res.send(`Command output: ${stdout}`);
+      try {
+        const oneTimeUrl = await generateOneTimeUrl('FLAG-{32CharactersA-Za-z0-9}');
+        res.send(`One-time URL to retrieve the flag: ${oneTimeUrl}`);
+      } catch (error) {
+        console.error('Error generating one-time URL:', error);
+        res.status(500).send('Error generating one-time URL');
+      }
     }
   });
+});
+
+app.get('/retrieve-flag', async (req, res) => {
+  const secretId = req.query.id;
+  try {
+    const secretValue = await secretsManager.getSecretValue({ SecretId: secretId }).promise();
+    res.send(`Flag: ${secretValue.SecretString}`);
+  } catch (error) {
+    console.error('Error retrieving flag:', error);
+    res.status(500).send('Error retrieving flag');
+  }
 });
 
 app.listen(port, () => {
